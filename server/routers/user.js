ab398c8eb7bf0108b462a06689482815a6ae5e04
turoyal <anhturoyal@gmail.com>
2023-04-17 10:49:04 +0700

    anhtu

--- server/routers/user.js
+++ server/routers/user.js
@@ -4,4 +4,6 @@ const middlewreController = require('../controllers/middlewareController')
 router.get("/",middlewreController.verifyToken,userController.getAlluser)
 router.delete("/:id",middlewreController.verifyTokenandadmin,userController.deleteUser)
 router.put("/:id",middlewreController.verifyToken,userController.ediUser)
+router.get("/getuserlogin", userController.getuserlogin)
+router.patch('/updatelike', userController.updatelike)
 module.exports = router
